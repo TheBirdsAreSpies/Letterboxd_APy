f791b394c88c6c6fa5ebad00a380dda548b8c187
Fabian Bauer <shore@skreamyourlungsout.com>
2023-11-26 16:15:18 +0100

    DiaryEntry should extend movie

--- diary_entry.py
+++ diary_entry.py
@@ -1,11 +1,13 @@
-class Diary_Entry:
+from letterboxd_apy.movie import Movie
+
+
+class DiaryEntry(Movie):
     def __init__(self, date, title, rating, is_rewatch, is_liked, review, tags, slug, viewing_id):
+        super().__init__(slug, title)
         self.date = date
-        self.title = title
         self.viewing_id = viewing_id
         self.rating = rating
         self.is_rewatch = is_rewatch
         self.is_liked = is_liked
         self.review = review
         self.tags = tags
-        self.slug = slug
